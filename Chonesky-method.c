ee05db51221db7224f05fe55988888e94f9a93db
chrom-P <kkllmm104@gmail.com>
2022-02-13 03:42:59 +0900

    up

--- Chonesky-method.c
+++ Chonesky-method.c
@@ -6,9 +6,9 @@
 int main(void)
 {
     int i,j,k;
-    float A[N][N],L[N][N],U[N][N],x[N],b[N],y[N];
+    float I,A[N][N],L[N][N],U[N][N],x[N],b[N],y[N];
     FILE *fp;
-    if( (fp=fopen("20191105-2.dat","r"))==NULL ) {
+    if( (fp=fopen("text.txt","r"))==NULL ) {
         printf("cannot open file.\n");
         exit(0);
     }
@@ -30,14 +30,18 @@ int main(void)
         }
         x[i]=0.0;
     }
-/* A=LU (U=LT) -> L,U */
+
+
     for(j=0;j<N;j++) {
         for(i=j;i<N;i++) {
             if(i==j) {
             L[i][j]=A[i][j];
             for(k=0;k<i;k++)
                 L[i][j]-=L[i][k]*L[i][k];
-            L[i][j]=sqrt(L[i][j]);
+                if(L[i][j]<0)
+                    L[i][j]=sqrt(L[i][j]*-1)*I;
+                else
+                    L[i][j]=sqrt(L[i][j]);
             } else {
                 L[i][j]=A[i][j];
                 for(k=0;k<j;k++)
@@ -48,24 +52,35 @@ int main(void)
         }
     }
     for(i=0;i<N;i++){
-        for(j=0;j<N;j++) printf(" %6.2f",L[i][j]);
+        for(j=0;j<N;j++) printf(" %6.2f+(%6.2f)i",L[i][j],L[i][j]);
         printf("\t");
-        for(j=0;j<N;j++) printf(" %6.2f",U[i][j]);
+        for(j=0;j<N;j++) printf(" %6.2f+(%6.2f)i",U[i][j],U[i][j]);
         printf("\n");
     }
     printf("\n");
-/* Ly=b -> y */
     for(i=0;i<N;i++) {
         y[i]=b[i];
         for(j=0;j<i;j++) y[i]-=L[i][j]*y[j];
         y[i]/=L[i][i];
     }
-/* Ux=y -> x */
     for(i=N-1;i>=0;i--) {
         x[i]=y[i];
         for(j=i+1;j<N;j++) x[i]-=U[i][j]*x[j];
         x[i]/=U[i][i];
     }
-/* result */
     for(i=0;i<N;i++) printf("%6.2f\n",x[i]);
 }
+
+/*
+   4.00   2.00   6.00	 30.00
+   2.00  17.00  -1.00	 19.00
+   6.00  -1.00  14.00	 56.00
+
+   2.00+(  0.00)i   0.00+(  0.00)i   0.00+(  0.00)i	   2.00+(  0.00)i   1.00+(  0.00)i   3.00+(  0.00)i
+   1.00+(  0.00)i   4.00+(  0.00)i   0.00+(  0.00)i	   0.00+(  0.00)i   4.00+(  0.00)i  -1.00+(  0.00)i
+   3.00+(  0.00)i  -1.00+(  0.00)i   2.00+(  0.00)i	   0.00+(  0.00)i   0.00+(  0.00)i   2.00+(  0.00)i
+
+  2.50
+  1.00
+  3.00
+*/
